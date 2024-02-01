88f86cc2371b8a7f19396c0ac8829a89d3400d9c
Sean Soon <secas20@bath.ac.uk>
2024-02-01 11:00:12 +0000

    Edited user_input.py

--- src/user_input.py
+++ src/user_input.py
@@ -4,29 +4,27 @@ from datetimerange import DateTimeRange
 
 # PRESET_BOOKING_LIST is ordered Mon - Sun.
 # Do not change the dates from 1900-01-01.
-# If you have multiple preferred slots in a day, list them according to order of preference
+# If you have multiple preferred slots in a day, list them from most to least preferred.
 NO_BOOKING = DateTimeRange('1900-01-01T00:00:00', '1900-01-01T00:00:00')
 USER_BOOKING_LIST = [
     [
         NO_BOOKING
     ],
     [
-        DateTimeRange('1900-01-01T16:00:00', '1900-01-01T17:00:00')
+        DateTimeRange('1900-01-01T09:00:00', '1900-01-01T11:00:00'),
+        DateTimeRange('1900-01-01T12:00:00', '1900-01-01T14:00:00')
     ],
     [
         DateTimeRange('1900-01-01T09:00:00', '1900-01-01T11:00:00')
     ],
     [
-        DateTimeRange('1900-01-01T10:15:00', '1900-01-01T11:00:00')
+        DateTimeRange('1900-01-01T09:00:00', '1900-01-01T11:00:00')
     ],
     [
-        # DateTimeRange('1900-01-01T15:15:00', '1900-01-01T16:00:00'),
-        # DateTimeRange('1900-01-01T11:15:00', '1900-01-01T12:00:00'),
-        # DateTimeRange('1900-01-01T09:00:00', '1900-01-01T10:00:00')
         NO_BOOKING
     ],
     [
-        DateTimeRange('1900-01-01T15:45:00', '1900-01-01T17:45:00')
+        DateTimeRange('1900-01-01T09:00:00', '1900-01-01T11:00:00')
     ],
     [
         NO_BOOKING
@@ -39,13 +37,16 @@ ROOM_PREFERENCE_LIST = ['room 2', 'room 1', 'buchan', 'choral']
 
 # Your UoB username.
 USERNAME = ''
+
 # If you have registered with the Edge using an email other than your UoB email, enter it below.
 EMAIL = f'{USERNAME}@bath.ac.uk'
+
 # The password for your Edge account.
 PASSWORD = ''
-# As per the booking form, this will be your UoB username by deault.
+
+# As per the booking form, this will be your UoB username by default.
 BOOKING_DESCRIPTION = USERNAME
 
 # The minimum and maximum length you would like your bookings to be.
-MIN_BOOKING_LENGTH = timedelta(hours=1, minutes=0)
+MIN_BOOKING_LENGTH = timedelta(hours=0, minutes=30)
 MAX_BOOKING_LENGTH = timedelta(hours=2, minutes=0)
